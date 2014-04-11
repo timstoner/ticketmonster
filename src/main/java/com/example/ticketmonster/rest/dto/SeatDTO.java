238768b748f0d4ca869600bf0350b3c22564144b
Tim Stoner <timothy.m.stoner@gmail.com>
2014-04-11 06:12:38 -0400

    added ticketmonster

---
+++ src/main/java/com/example/ticketmonster/rest/dto/SeatDTO.java
@@ -0,0 +1,69 @@
+package com.example.ticketmonster.rest.dto;
+
+import java.io.Serializable;
+
+import com.example.ticketmonster.model.Seat;
+import com.example.ticketmonster.rest.dto.NestedSectionDTO;
+
+import javax.persistence.EntityManager;
+
+public class SeatDTO implements Serializable
+{
+
+   private int number;
+   private int rowNumber;
+   private NestedSectionDTO section;
+
+   public SeatDTO()
+   {
+   }
+
+   public SeatDTO(final Seat entity)
+   {
+      if (entity != null)
+      {
+         this.number = entity.getNumber();
+         this.rowNumber = entity.getRowNumber();
+         this.section = new NestedSectionDTO(entity.getSection());
+      }
+   }
+
+   public Seat fromDTO(Seat entity, EntityManager em)
+   {
+      if (entity == null)
+      {
+         entity = new Seat();
+      }
+      return entity;
+   }
+
+   public int getNumber()
+   {
+      return this.number;
+   }
+
+   public void setNumber(final int number)
+   {
+      this.number = number;
+   }
+
+   public int getRowNumber()
+   {
+      return this.rowNumber;
+   }
+
+   public void setRowNumber(final int rowNumber)
+   {
+      this.rowNumber = rowNumber;
+   }
+
+   public NestedSectionDTO getSection()
+   {
+      return this.section;
+   }
+
+   public void setSection(final NestedSectionDTO section)
+   {
+      this.section = section;
+   }
+}
